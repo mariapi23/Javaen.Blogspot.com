a4b2b715f07d3b6c19a89a1e83f160c4bd9ea10c
maria <maria.pi.23.55@gmail.com>
2017-10-26 16:42:15 +0200

    Segundo commit

--- src/com/javaen/HolaMundo/HolaMundo.java
+++ src/com/javaen/HolaMundo/HolaMundo.java
@@ -3,6 +3,7 @@ package com.javaen.HolaMundo;
 public class HolaMundo {
 	public static void main (String [] args){
 		System.out.println("Hola mundo en Java");
+		System.out.println("Subida a git conseguida");
 	}
 
 }
